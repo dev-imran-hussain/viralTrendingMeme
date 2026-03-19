a4697642ad78bfd1e5912fe4385037f57a73de81
dev-Imran-Hussain <emuhussain721@gmail.com>
2026-03-19 13:55:24 +0530

    add google analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-363FYJD978" 
+          strategy="afterInteractive" 
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-363FYJD978');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
